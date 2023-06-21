ec1fe02345d87f873cbc6cbe6b6b0f2b74f4396e
Natalie Ben Haim <benhaimnatalie@gmail.com>
2023-06-21 15:58:39 +0300

    second-assignment

--- src/app/Components/board/board.component.ts
+++ src/app/Components/board/board.component.ts
@@ -7,12 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   title = 'Battleship Game';
-  board: (number | string)[][] = [];   //????????????????????????????
+  board: (string)[][] = []; 
   boardSize: number = 10;
   ships: number = 10;
-  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+  letters: Array<string> = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
   gameOver: boolean = false;
   hitCounter: number = 0;
+  shipsArray: Array<number> = [5, 5, 4, 4, 3, 3, 2, 2, 1, 1];
+  shipsLocationArray: Array<any> = [];
+  isCellEmpty: boolean = false;
+  sumArrayShips: number = 0;
 
   ngOnInit() {
     this.startGame();
@@ -24,26 +28,69 @@ export class BoardComponent implements OnInit {
     this.gameOver = false;
     this.hitCounter = 0;
     for (let i = 0; i < this.boardSize; i++) {
-      let row: (number | string)[] = [];
+      let row: (string)[] = [];
       for (let j = 0; j < this.boardSize; j++) {
         row.push("");
       }
       this.board.push(row);
     }
-    this.placeShips();
+    // count ship cells
+    this.sumArrayShips = this.shipsArray.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
+    this.findRandomDots();
   }
 
   //  Placing the ships randomly on the board
-  placeShips() {
-    let counter = 0;
-    while (this.ships > counter) {
-      let row = Math.floor(Math.random() * 10);
-      let col = Math.floor(Math.random() * 10);
-      // check duplicates:
-      if (this.board[row][col] !== 1) {         //1=ship in cell
-        this.board[row][col] = 1;
-        counter++;
+  // placeShips() {
+  findRandomDots() {
+    let randomStartIndex = Math.floor(Math.random() * this.boardSize);
+    this.shipsArray.forEach((ship) => {
+      let validColStart = randomStartIndex <= this.boardSize - ship;
+      if (validColStart) {
+        let row = Math.floor(Math.random() * 10);
+        this.checkEmptyCell(row, randomStartIndex, ship);
+      } else {
+        this.findRandomDots();
       }
+    });
+  }
+
+  checkEmptyCell(row: number, validColStart: number, ship: number) {
+    for (let i = 0; i < ship; i++) {
+      if (this.board[row][validColStart + i].includes('ship')) {
+        this.isCellEmpty = false;
+      }
+    }
+    if (this.isCellEmpty) {
+      this.placeOneShip(row, validColStart, ship);
+    }
+    else {
+      this.isCellEmpty = true;
+      this.findRandomDots();
+    }
+  }
+
+  placeOneShip(row: number, validColStart: number, ship: number) {
+    // adding ship
+    for (let i = 0; i < ship; i++) {
+      this.board[row][validColStart + i] = 'ship';
+
+      // adding classes for different border:
+      if (i === 0 || ship === 1) {
+        this.board[row][validColStart + i] = 'ship ship-start';
+      } else if (i === ship - 1) {
+        this.board[row][validColStart + i] = 'ship ship-end';
+      }
+      if (ship === 1) {
+        this.board[row][validColStart + i] = 'ship ship-one';
+      }
+    }
+    // remove ship from shipsArray after placing it on board
+    let shipIndex = this.shipsArray.indexOf(ship);
+    this.shipsArray.splice(shipIndex, 1);
+    if (this.shipsArray.length !== 0) {
+      this.findRandomDots();
     }
   }
 
@@ -55,7 +102,7 @@ export class BoardComponent implements OnInit {
     }
     // checking if cell is with ship
     if (!this.gameOver) {
-      if (this.board[i][j] === 1) {
+      if (this.board[i][j].includes("ship")) {
         this.board[i][j] = 'hit';
         this.hitCounter++;
         this.checkGameOver();
@@ -68,23 +115,9 @@ export class BoardComponent implements OnInit {
 
   // after hitting a ship, checking if the game is over
   checkGameOver() {
-    if (this.hitCounter === this.ships) {
+    if (this.hitCounter === this.sumArrayShips) {
       this.gameOver = true;
     }
   }
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
